be29743b840f6d180acd4dd7c9b254d7af886bfc
LiamBartolini <liambartolini@gmail.com>
2021-01-13 18:14:54 +0100

    first commit

---
+++ Bartolini.Liam.4h.RiassuntoSkia/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Bartolini.Liam._4h.RiassuntoSkia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Bartolini.Liam.4h.RiassuntoSkia/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SkiaSharp;
+using System.IO;
+using Xamarin.Essentials;
+
+namespace Bartolini.Liam._4h.RiassuntoSkia
+{
+    public partial class MainPage : ContentPage
+    {
+        public int margineSinistro { get; set; } = 100;
+        public int margineSuperiore { get; set; } = 100;
+        public int larghezzaRettangolo { get; set; } = 200;
+        public int altezzaRettangolo { get; set; } = 300;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Tela(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs e)
+        {
+            var info = e.Info;
+            var canvas = e.Surface.Canvas;
+            canvas.Clear();
+
+            int larghezza = info.Rect.Width;
+            int altezza = info.Rect.Height;
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.BlueViolet,
+                StrokeWidth = 3
+            };
+
+            SKPath rettangolo = AreaDelRettangolo();
+
+            canvas.DrawPath(rettangolo, paint);
+
+            TelaDaDisegno.InvalidateSurface();
+        }
+
+        SKPath AreaDelRettangolo()
+        {
+            SKPath rettangolo = new SKPath();
+
+            rettangolo.MoveTo(margineSinistro, margineSuperiore);
+            rettangolo.LineTo(margineSinistro + larghezzaRettangolo, margineSuperiore);
+            rettangolo.LineTo(margineSinistro + larghezzaRettangolo, margineSuperiore + altezzaRettangolo);
+            rettangolo.LineTo(margineSinistro, margineSuperiore + altezzaRettangolo);
+            rettangolo.LineTo(margineSinistro, margineSuperiore);
+
+            return rettangolo;
+        }
+
+        private void btnDisegna_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
